322d17cde97b6bd218214172620fcbbfa56ee9ad
udamasa0616 <udamasa0616@outlook.jp>
2022-05-03 08:50:12 +0900

    イベント発火　基礎編（再提出２）

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener('load',function(){
+
+  const pullDownButton = document.getElementById("lists");
+  console.log (pullDownButton);
+});
+
+window.addEventListener('click',function(){
+
+  const pullDownButton = document.getElementById("lists");
+  console.log (pullDownButton);
+});
+
+window.addEventListener('mouseover',function(){
+
+  const pullDownButton = document.getElementById("lists");
+  console.log (pullDownButton);
+});
+
+window.addEventListener('mauseout',function(){
+
+  const pullDownButton = document.getElementById("lists");
+  console.log (pullDownButton);
+});
