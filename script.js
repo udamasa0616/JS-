4b15aba3aa644482ca7f768ed9c969138915773a
udamasa0616 <udamasa0616@outlook.jp>
2022-05-12 07:41:12 +0900

    インラインスタイル編
    インラインスタイル　追加
    コメント　追記

--- script.js
+++ script.js
@@ -7,17 +7,30 @@ window.addEventListener('load',function(){
 window.addEventListener('click',function(){
 
   const pullDownButton = document.getElementById("lists");
+  pullDownButton.addEventListener("click",function(){
+    pullDownButton.setAttribute("style","background-color:green;");
+  });
   console.log (pullDownButton);
 });
 
 window.addEventListener('mouseover',function(){
 
   const pullDownButton = document.getElementById("lists");
+  pullDownButton.addEventListener("mouseover",function(){
+  pullDownButton.setAttribute("style","background-color:blue;");
+});
   console.log (pullDownButton);
 });
 
-window.addEventListener('mauseout',function(){
-
+// ウインドウを読み込んだ時のイベント
+window.addEventListener('mouseout',function(){
+  // HTMLより読み込むID要素の設定。クラス,セレクタの場合は.querySelecton
   const pullDownButton = document.getElementById("lists");
+  // ウインドウを読み込んだ後のイベント
+  pullDownButton.addEventListener("mouseout",function(){
+  pullDownButton.removeAttribute("style","background-color:red;");
+});
   console.log (pullDownButton);
 });
+
+
