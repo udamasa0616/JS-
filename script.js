9cb81c959a9b50ca82c75913557fe814519c8781
udamasa0616 <udamasa0616@outlook.jp>
2022-05-18 21:22:52 +0900

    インラインスタイル編（再提出）
    HTML：scriptの位置を変えた。
    問題点：<heder>scriptが先に読まれていてHTML後に読み込まれる。修正。
    index,.js：修正。複雑に考えすぎていたため、修正。

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 // ウインドウを読み込んだ時のイベント
-window.addEventListener('load',function(){
-// HTMLより読み込むID要素の設定。クラス,セレクタの場合は.querySelecton
-  const pullDownButton = document.getElementById("lists");
-  console.log (pullDownButton);
+//window.addEventListener('load',function(){
+// HTMLより読み込むID要素の設定。クラス,セレクタの場合は.querySelecton()
+// ウインドウを読み込んだ後のイベント
+//console.log (pullDownButton);
+//});
+const pullDownButton = document.getElementById("lists");
+
+pullDownButton.addEventListener('click',function(){
+    pullDownButton.setAttribute("style","background-color:green;");
+    console.log (pullDownButton);
+  });
+
+pullDownButton.addEventListener('mouseover',function(){
+    pullDownButton.setAttribute("style","background-color:blue;");
+    console.log (pullDownButton);
 });
 
-window.addEventListener('click',function(){
-
-  const pullDownButton = document.getElementById("lists");
-  console.log (pullDownButton);
-});
-
-window.addEventListener('mouseover',function(){
-
-  const pullDownButton = document.getElementById("lists");
-  console.log (pullDownButton);
-});
-
-
-window.addEventListener('mouseout',function(){
-  
-  const pullDownButton = document.getElementById("lists");
-  // ウインドウを読み込んだ後のイベント
-  console.log (pullDownButton);
-});
+pullDownButton.addEventListener('mouseout',function(){
+    pullDownButton.removeAttribute("style","background-color:red;");
+    console.log (pullDownButton);
+  });
 
 
