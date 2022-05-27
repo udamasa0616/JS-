e1f6a04d7a68714f5dfb1e3824a9f454d8579dce
udamasa0616 <101563907+udamasa0616@users.noreply.github.com>
2022-05-27 22:36:51 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 // ウインドウを読み込んだ時のイベント
 //window.addEventListener('load',function(){
-// HTMLより読み込むID要素の設定。クラス,セレクタの場合は.querySelecton()
+// HTMLより読み込むID要素の設定。クラス,セレクタの場合は.querySelector()
 // ウインドウを読み込んだ後のイベント
 //console.log (pullDownButton);
 //});
-const pullDownButton = document.getElementById("lists");
+const pullDownButton  = document.getElementById("lists");
+const pullDownParents = document.getElementById("pull-down");
 
-pullDownButton.addEventListener('click',function(){
-    pullDownButton.setAttribute("style","background-color:green;");
-    console.log (pullDownButton);
+pullDownButton.addEventListener('click', function() {
+    //pullDownButton.setAttribute("style","background-color:green;");                
+    //console.log (pullDownButton);
+    if(pullDownParents.getAttribute('style') == 'display:block;'){
+    pullDownParents.removeAttribute("style", "display:block;");
+    console.log('非表示');
+    }else {
+      pullDownParents.setAttribute('style', 'display:block;');
+      console.log('表示');
+    };
   });
 
 pullDownButton.addEventListener('mouseover',function(){
-    pullDownButton.setAttribute("style","background-color:blue;");
+    this.setAttribute("style","background-color:blue;");
     console.log (pullDownButton);
 });
 
 pullDownButton.addEventListener('mouseout',function(){
-    pullDownButton.removeAttribute("style","background-color:red;");
+    this.removeAttribute("style","background-color:red;");
     console.log (pullDownButton);
   });
-
 
