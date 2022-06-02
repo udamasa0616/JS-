db09ed8d9b170c11a98b6bf538532c984e72b5f1
udamasa0616 <101563907+udamasa0616@users.noreply.github.com>
2022-06-02 22:25:58 +0900

    文字列の取得編

--- script.js
+++ script.js
@@ -4,8 +4,11 @@
 // ウインドウを読み込んだ後のイベント
 //console.log (pullDownButton);
 //});
+function pullDown(){
 const pullDownButton  = document.getElementById("lists");
 const pullDownParents = document.getElementById("pull-down");
+const pullDownChild   = document.querySelectorAll('.pull-down-list');
+const currentList     = document.getElementById('current-list');
 
 pullDownButton.addEventListener('click', function() {
     //pullDownButton.setAttribute("style","background-color:green;");                
@@ -17,7 +20,14 @@ pullDownButton.addEventListener('click', function() {
       pullDownParents.setAttribute('style', 'display:block;');
       console.log('表示');
     };
+    
+    pullDownChild.forEach(function(list) {
+      list.addEventListener('click', function() {
+        const value = list.innerHTML
+        console.log(value);
+        currentList.innerHTML = value
   });
+});
 
 pullDownButton.addEventListener('mouseover',function(){
     this.setAttribute("style","background-color:blue;");
@@ -27,5 +37,21 @@ pullDownButton.addEventListener('mouseover',function(){
 pullDownButton.addEventListener('mouseout',function(){
     this.removeAttribute("style","background-color:red;");
     console.log (pullDownButton);
-  });
+  })
+});
+};
+window.addEventListener('load',pullDown);
 
+// pullDownChild.forEach(function(list) {
+//   list.addEventListener('click', function() {
+//     const value = list.innerHTML
+//     console.log(value);
+//     currentList.innerHTML = value
+//   if(currentList.innerHTML == 0){
+//         let first = document.createElement('a');
+//         first.setAttribute('id', 'first');
+//         first.insertBefore('href','https://www.javadrive.jp/javascript/array/index10.html');
+//         pullDownChild.insertBefore(first, null);
+//   }
+// });
+// });
